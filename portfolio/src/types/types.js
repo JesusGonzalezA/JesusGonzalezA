5651f096a0a0d9fb7d75087c46d7b4f5ac68575d
Jesús González Álvarez <51116828+JesusGonzalezA@users.noreply.github.com>
2021-07-06 14:45:28 +0200

    Updates

--- portfolio/src/types/types.js
+++
@@ -1,9 +0,0 @@
-
-export const types = {
-
-    // UI
-    toggleTheme: '[UI] Toggle theme',
-    toggleBurgerMenu: '[UI] Toggle burger menu',
-    setBurgerMenuState: '[UI] Set burger menu state',
-    setHeaderHeight: '[UI] Set header height'
-}
